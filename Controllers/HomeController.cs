61b4537b6aabb08d97690b821f79561972d76f59
akiman410 <98837184+akiman410@users.noreply.github.com>
2022-03-02 10:09:37 -0500

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using Infrastructure.Services;
+using Microsoft.AspNetCore.Mvc;
+using MovieShopMVC.Models;
+using System.Diagnostics;
+
+namespace MovieShopMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            // Our Controllers are very thin/lean
+            // most of your logic should come from other dependancies, services
+            //Interfaces
+
+            var movieService = new MovieService();
+            var movies = movieService.GetTop30GrossingMovies();
+            return View(movies);
+        }
+        [HttpGet]
+        
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        [HttpGet]
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}

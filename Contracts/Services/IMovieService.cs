3dfa36dd8aef4c3483ad8c411e130845e7df1930
akiman410 <98837184+akiman410@users.noreply.github.com>
2022-03-02 10:03:33 -0500

    Add files via upload

---
+++ Contracts/Services/IMovieService.cs
@@ -0,0 +1,15 @@
+﻿using ApplicationCore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationCore.Contracts.Services
+{
+    public interface IMovieService
+    {
+        //Have all the business logic methods relating to Movies
+        List<MovieCardModel> GetTop30GrossingMovies();
+    }
+}
